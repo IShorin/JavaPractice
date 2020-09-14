1823add83eb3ba6ce31afb60a9611b4cf1c9e7c2
IShorin <ishorin@icloud.com>
2020-09-14 12:01:52 +0300

    3.2 done

--- src/ru/IShorin/JavaPractice/laba3/laba3.java
+++ src/ru/IShorin/JavaPractice/laba3/laba3.java
@@ -1,5 +1,5 @@
 package ru.IShorin.JavaPractice.laba3;
-
+//////////////////////Task 1
 class Circle {
     private int d;
 
@@ -25,3 +25,46 @@ class CircleTest {
     }
 
 }
+/////////////////////////// Task 2
+class Human{
+
+
+    private Head obx=new Head();
+    private Leg obx1=new Leg();
+    private Hand obx2=new Hand();
+
+}
+class Head{
+    private int a=5;
+    public int getHead() {
+        return a;
+    }
+
+    public void setHead(int a) {
+        this.a = a;
+    }
+}
+class Leg{
+    private int b=10;
+    public int getLeg() {
+        return b;
+    }
+
+    public void setLeg(int b) {
+        this.b = b;
+    }
+}
+
+class Hand{
+    private int c=5;
+    public int getHand() {
+        return c;
+    }
+
+    public void setHand(int c) {
+        this.c = c;
+    }
+}
+/////////////////////////Task 3
+
+
