878c6f1efe0a735359bc528cf73a53f920dd06bb
IShorin <ishorin@icloud.com>
2020-09-19 18:16:46 +0300

    laba4 done

---
+++ src/ru/IShorin/JavaPractice/laba4/laba4.java
@@ -0,0 +1,153 @@
+package ru.IShorin.JavaPractice.laba4;
+
+abstract class Shape {
+    protected String color;
+    protected boolean filled;
+
+    public Shape() {
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+    abstract double getArea();
+
+    abstract double getPerimeter();
+
+}
+
+abstract class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+    abstract double getArea();
+
+    abstract double getPerimeter();
+}
+
+abstract class Rectangle extends Shape {
+    private double width;
+    private double length;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(String color, boolean filled, double width, double length) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+    abstract double getArea();
+
+    abstract double getPerimeter();
+}
+
+abstract class Square extends Shape{
+    public double side;
+
+    public Square() {
+    }
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public Square(String color, boolean filled, double side) {
+        super(color, filled);
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+    abstract void setWidth(double side);
+
+    abstract void setLength(double side);
+}
+
+
